1d8e82cc5288813b56f6ff5807cb50c630f51e50
Alexandre Henrique <alexandre_henrique98@hotmail.com>
2020-11-27 13:30:24 -0300

    Payment one-to-one associacao

--- src/main/java/com/javaweb/javaweb/config/TesteConfig.java
+++ src/main/java/com/javaweb/javaweb/config/TesteConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.javaweb.javaweb.entities.Category;
 import com.javaweb.javaweb.entities.Order;
 import com.javaweb.javaweb.entities.OrderItem;
+import com.javaweb.javaweb.entities.Payment;
 import com.javaweb.javaweb.entities.Product;
 import com.javaweb.javaweb.entities.User;
 import com.javaweb.javaweb.entities.enums.OrderStatus;
@@ -80,6 +81,10 @@ public class TesteConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+		Payment pay1 = new Payment(Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 
 }
